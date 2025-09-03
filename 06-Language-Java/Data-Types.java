8f16d3489ec6a9bcabdaef0b8a8c417021f6e392
LINGKON ISLAM <lingkon707@gmail.com>
2025-09-04 00:22:19 +0600

    Create Data-Types.java

---
+++ 06-Language-Java/Data-Types.java
@@ -0,0 +1,20 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // Primitive types
+        int age = 25;
+        double salary = 55000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        // Non-primitive types
+        String name = "Lingkon";
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("First number in array: " + numbers[0]);
+    }
+}
