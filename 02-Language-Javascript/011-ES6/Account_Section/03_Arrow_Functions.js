39668502175dc29f1c65d4af2c476a5f2490fe72
LINGKON ISLAM <lingkon707@gmail.com>
2025-09-29 22:58:40 +0600

    Create 03_Arrow_Functions.js

---
+++ 02-Language-Javascript/011-ES6/Account_Section/03_Arrow_Functions.js
@@ -0,0 +1,9 @@
+// Quickly calculate totals or taxes.
+
+const calculateTax = (amount) => amount * 0.05; // 5% tax
+console.log(calculateTax(2000)); // 100
+
+// Summing invoice amounts
+const invoices = [100, 200, 300];
+const total = invoices.reduce((sum, invoice) => sum + invoice, 0);
+console.log(total); // 600
