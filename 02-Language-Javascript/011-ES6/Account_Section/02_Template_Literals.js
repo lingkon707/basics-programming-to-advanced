f951c5ed58beca096983219cdc09ee09fd98ff2f
LINGKON ISLAM <lingkon707@gmail.com>
2025-09-29 22:57:15 +0600

    Create 02_Template_Literals.js

---
+++ 02-Language-Javascript/011-ES6/Account_Section/02_Template_Literals.js
@@ -0,0 +1,7 @@
+const invoiceNumber = 101;
+const clientName = "John Doe";
+const amountDue = 1500;
+
+const invoiceMessage = `Invoice #${invoiceNumber} for ${clientName} is $${amountDue}`;
+console.log(invoiceMessage);
+// Invoice #101 for John Doe is $1500
