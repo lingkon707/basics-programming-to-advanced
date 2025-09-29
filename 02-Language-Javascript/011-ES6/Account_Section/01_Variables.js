8fa2892ab2d8ff3d4763b0fad11821f86154352f
LINGKON ISLAM <lingkon707@gmail.com>
2025-09-29 22:56:04 +0600

    Create Account_Section/01_Variables.js
    ES6 on Private company

---
+++ 02-Language-Javascript/011-ES6/Account_Section/01_Variables.js
@@ -0,0 +1,9 @@
+// Total revenue for the month
+let totalRevenue = 50000;
+
+// Company name stays constant
+const companyName = "Acme Corp";
+
+// We can update totals
+totalRevenue += 10000; // Received extra payment
+console.log(totalRevenue); // 60000
