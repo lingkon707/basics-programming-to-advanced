0acdd460e7fc1d4db52a1a2432a7d9d415f65378
LINGKON ISLAM <lingkon707@gmail.com>
2025-09-30 13:13:21 +0600

    Delete 02-Language-Javascript/011-ES6/Account_Section directory

--- 02-Language-Javascript/011-ES6/Account_Section/01_Variables.js
+++
@@ -1,9 +0,0 @@
-// Total revenue for the month
-let totalRevenue = 50000;
-
-// Company name stays constant
-const companyName = "Acme Corp";
-
-// We can update totals
-totalRevenue += 10000; // Received extra payment
-console.log(totalRevenue); // 60000
