6d2ce308fcb11cd1195a0f43a3df5105c8726af0
LINGKON ISLAM <lingkon707@gmail.com>
2025-10-12 20:06:29 +0600

    Create decisionAgeGender.js

---
+++ 02-Language-Javascript/Master-In-JS/decisionAgeGender.js
@@ -0,0 +1,16 @@
+let people = [
+  { name: "Amina", gender: "female", age: 62 },
+  { name: "Rahim", gender: "male", age: 65 },
+  { name: "Sara", gender: "female", age: 45 },
+  { name: "Kamal", gender: "male", age: 58 }
+];
+
+for (let person of people) {
+  if (person.age >= 60 && person.gender.toLowerCase() === "female") {
+    console.log(person.name + ": Senior female citizen, eligible for special benefits.");
+  } else if (person.age >= 60 && person.gender.toLowerCase() === "male") {
+    console.log(person.name + ": Senior male citizen, eligible for senior benefits.");
+  } else {
+    console.log(person.name + ": Not eligible for senior benefits yet.");
+  }
+}
