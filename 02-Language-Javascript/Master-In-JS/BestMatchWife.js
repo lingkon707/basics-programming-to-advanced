8221708cf2ce67fc2cce548ea8395ede2ae90cf0
LINGKON ISLAM <lingkon707@gmail.com>
2025-10-26 23:52:09 +0600

    Create BestMatchWife.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/BestMatchWife.js
@@ -0,0 +1,8 @@
+const wifeList = [{name:"Aisha",age:20,beauty:9},{name:"Fatima",age:22,beauty:7},{name:"Nabila",age:19,beauty:10}];
+const myChoice = {age:20,beauty:8};
+
+
+const bestMatch = wifeList.find(w=>w.age===myChoice.age && w.beauty>=myChoice.beauty);
+
+
+console.log(bestMatch ? `Perfect match: ${bestMatch.name}` : "No match found!");
