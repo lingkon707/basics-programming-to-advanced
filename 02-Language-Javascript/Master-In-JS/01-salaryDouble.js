4c237a2e0ba9f8b5c136ea1144d69124eb0c66d5
LINGKON ISLAM <lingkon707@gmail.com>
2025-11-03 23:29:17 +0600

    Create 01-salaryDouble.js
    hmm

---
+++ 02-Language-Javascript/Master-In-JS/01-salaryDouble.js
@@ -0,0 +1,11 @@
+const account = {
+  name: "John Doe",
+  salary: 30000,
+  accountAgeYears: 2 // change this to test
+};
+
+if (account.accountAgeYears > 1) {
+  account.salary *= 2; // double salary
+}
+
+console.log(`Updated Salary: ${account.salary}`);
