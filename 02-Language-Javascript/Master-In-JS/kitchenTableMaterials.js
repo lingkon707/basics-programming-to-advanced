314272dec255758fb87c66b7e2fa4b901819be74
LINGKON ISLAM <lingkon707@gmail.com>
2025-11-08 23:56:09 +0600

    Create kitchenTableMaterials.js
    yes

---
+++ 02-Language-Javascript/Master-In-JS/kitchenTableMaterials.js
@@ -0,0 +1,13 @@
+const kitchenTableMaterials = [
+  "Wood",
+  "Glass",
+  "Marble",
+  "Granite",
+  "Stainless Steel",
+  "Laminate",
+  "Concrete",
+  "Bamboo"
+];
+
+console.log("Kitchen Table Materials:");
+console.log(kitchenTableMaterials);
