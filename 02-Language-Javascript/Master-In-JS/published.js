562e0bfd99c8e796d18bf25fc9e2307dc363a016
LINGKON ISLAM <lingkon707@gmail.com>
2025-11-09 23:21:57 +0600

    Create published.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/published.js
@@ -0,0 +1,4 @@
+var punishments = ["Detention", "Extra Homework", "Writing Lines", "Verbal Warning", "Parent Meeting"];
+for(var i = 0; i < punishments.length; i++){
+    console.log(punishments[i]);
+}
