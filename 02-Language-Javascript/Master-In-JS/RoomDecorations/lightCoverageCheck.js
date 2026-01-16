f5f308d9679170da11a5581309fbafb48b49ae03
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-16 15:24:14 +0600

    Create lightCoverageCheck.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/RoomDecorations/lightCoverageCheck.js
@@ -0,0 +1,5 @@
+// 3 lightCoverageCheck.js
+var lightsInstalled = 18;
+var requiredLights = 20;
+
+console.log("Lighting coverage:", lightsInstalled >= requiredLights ? "Sufficient" : "Insufficient");
