d496cb32ce11ef9c1ebfbf8014f80cff460609d9
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-17 11:03:05 +0600

    Create colorThemeApproval.js
    js 2

---
+++ 02-Language-Javascript/Master-In-JS/RoomDecorations/colorThemeApproval.js
@@ -0,0 +1,9 @@
+// 5 colorThemeApproval.js
+var selectedColor = "White & Gold";
+var approvedColor = "White & Gold";
+
+console.log(
+  selectedColor === approvedColor
+  ? "Color theme approved"
+  : "Color theme rejected"
+);
