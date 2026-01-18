ba79867a907634b4ab7e9c0010047f03d21718bc
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-18 08:27:10 +0600

    Create decorationFinalApproval.js
    js-10th

---
+++ 02-Language-Javascript/Master-In-JS/RoomDecorations/decorationFinalApproval.js
@@ -0,0 +1,10 @@
+// 10 decorationFinalApproval.js
+var lightingOk = true;
+var flowersOk = true;
+var themeOk = true;
+
+console.log(
+  lightingOk && flowersOk && themeOk
+  ? "Room decoration: FINAL APPROVAL GRANTED"
+  : "Room decoration: FINAL APPROVAL DENIED"
+);
