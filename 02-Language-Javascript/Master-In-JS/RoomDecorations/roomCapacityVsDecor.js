7565552658a5b6e78e8f2a590f29f0e9db5f83db
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-18 08:26:46 +0600

    Create roomCapacityVsDecor.js
    js codes

---
+++ 02-Language-Javascript/Master-In-JS/RoomDecorations/roomCapacityVsDecor.js
@@ -0,0 +1,9 @@
+// 9 roomCapacityVsDecor.js
+var roomCapacity = 100;
+var decorationSpaceUsed = 25; // percent
+
+console.log(
+  decorationSpaceUsed <= 30
+  ? "Room space well balanced"
+  : "Decoration overcrowding detected"
+);
