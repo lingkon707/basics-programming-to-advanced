b7b62b1b35de11c67efe83c3e673f30d4a80cc9c
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-19 07:54:05 +0600

    Create guestImpressionScore.js
    js code

---
+++ 02-Language-Javascript/Master-In-JS/RoomDecorations/guestImpressionScore.js
@@ -0,0 +1,8 @@
+// 12 guestImpressionScore.js
+var feedbackScore = 4.6;
+
+console.log(
+  feedbackScore >= 4.5
+  ? "Guest impression: Outstanding decoration"
+  : "Guest impression: Needs improvement"
+);
