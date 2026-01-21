dbd3d6aeef1d4a6173bba05a7118e756a3c6bd6e
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-21 12:03:51 +0600

    Create 6_weakStudentIdentification.js
    js codes 2

---
+++ 02-Language-Javascript/Master-In-JS/Math_Teacher/6_weakStudentIdentification.js
@@ -0,0 +1,9 @@
+// 6_weakStudentIdentification.js
+var failedStudents = 12;
+
+console.log(
+  "Performance Analysis → Failed Students:",
+  failedStudents,
+  "| Intervention:",
+  failedStudents > 10 ? "EXTRA CLASSES MANDATORY" : "Monitor Closely"
+);
