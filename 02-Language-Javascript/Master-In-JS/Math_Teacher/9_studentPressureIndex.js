208b2ca1f561bea95d8ce3960d341effec5d033a
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-22 07:35:54 +0600

    Create 9_studentPressureIndex.js
    js code 2

---
+++ 02-Language-Javascript/Master-In-JS/Math_Teacher/9_studentPressureIndex.js
@@ -0,0 +1,9 @@
+// 9_studentPressureIndex.js
+var examsThisWeek = 4;
+
+console.log(
+  "Student Pressure Index → Exams:",
+  examsThisWeek,
+  "| Alert:",
+  examsThisWeek >= 4 ? "HIGH STRESS – COUNSELING ADVISED" : "Manageable"
+);
