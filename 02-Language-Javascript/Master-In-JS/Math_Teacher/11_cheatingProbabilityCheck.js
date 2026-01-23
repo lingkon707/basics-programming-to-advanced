ef4626ebc238034a96cb846d33dd7485cb14cf31
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-23 09:25:36 +0600

    Create 11_cheatingProbabilityCheck.js
    raw

---
+++ 02-Language-Javascript/Master-In-JS/Math_Teacher/11_cheatingProbabilityCheck.js
@@ -0,0 +1,9 @@
+// 11_cheatingProbabilityCheck.js
+var suspiciousStudents = 5;
+
+console.log(
+  "Exam Integrity Scan → Suspicious Cases:",
+  suspiciousStudents,
+  "| Action:",
+  suspiciousStudents > 3 ? "STRICT SURVEILLANCE ACTIVATED" : "Normal Watch"
+);
