32f62434752711404891669b790204fc5b03ab13
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-27 08:18:20 +0600

    Create 8_emergencyEvacuationDecision.js
    code

---
+++ 02-Language-Javascript/Master-In-JS/Examiner_Control_System/8_emergencyEvacuationDecision.js
@@ -0,0 +1,11 @@
+// 8_emergencyEvacuationDecision.js
+var emergencyAlert = true;
+
+console.log(
+  "EMERGENCY CONTROL → Alert Status:",
+  emergencyAlert,
+  "| COMMAND:",
+  emergencyAlert
+    ? "EVACUATE CENTER – EXAM SUSPENDED"
+    : "NO ACTION REQUIRED"
+);
