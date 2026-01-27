09845116061969d2ef4967c13f5f2360cb039e57
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-27 08:19:01 +0600

    Create 10_mediaLeakRiskAssessment.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/Examiner_Control_System/10_mediaLeakRiskAssessment.js
@@ -0,0 +1,11 @@
+// 10_mediaLeakRiskAssessment.js
+var leakReported = true;
+
+console.log(
+  "MEDIA & REPUTATION RISK → Leak Status:",
+  leakReported,
+  "| RESPONSE:",
+  leakReported
+    ? "PRESS CONTROL & LEGAL CELL ACTIVATED"
+    : "NO REPUTATIONAL THREAT"
+);
