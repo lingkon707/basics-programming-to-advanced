c1fa87b453c4264e193ef896f0152d4097a93c96
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-27 08:18:41 +0600

    Create 9_boardComplianceCheck.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/Examiner_Control_System/9_boardComplianceCheck.js
@@ -0,0 +1,9 @@
+// 9_boardComplianceCheck.js
+var rulesFollowed = false;
+
+console.log(
+  "BOARD COMPLIANCE REVIEW →",
+  rulesFollowed
+    ? "FULLY COMPLIANT"
+    : "NON-COMPLIANCE – EXAM AT RISK OF CANCELLATION"
+);
