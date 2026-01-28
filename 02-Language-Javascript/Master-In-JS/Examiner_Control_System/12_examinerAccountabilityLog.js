37603595d76ceab1790e09ec547ba7e93792da04
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-28 11:20:48 +0600

    Create 12_examinerAccountabilityLog.js
    js codes 2

---
+++ 02-Language-Javascript/Master-In-JS/Examiner_Control_System/12_examinerAccountabilityLog.js
@@ -0,0 +1,11 @@
+// 12_examinerAccountabilityLog.js
+var negligenceReports = 1;
+
+console.log(
+  "EXAMINER ACCOUNTABILITY → Negligence Reports:",
+  negligenceReports,
+  "| ACTION:",
+  negligenceReports > 0
+    ? "SHOW-CAUSE NOTICE ISSUED"
+    : "NO ACTION REQUIRED"
+);
