090e9879a8f38591de539c736ecc3c2ea2552141
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-31 18:28:17 +0600

    Create 11_civilAuthorityCoordination.js
    js

---
+++ 02-Language-Javascript/Master-In-JS/ArmyDefenseTasks/11_civilAuthorityCoordination.js
@@ -0,0 +1,9 @@
+// 11_civilAuthorityCoordination.js
+var coordinationStatus = false;
+
+console.log(
+  "CIVIL COORDINATION STATUS →",
+  coordinationStatus
+    ? "COORDINATION ESTABLISHED"
+    : "COMMUNICATION GAP – IMMEDIATE LIAISON REQUIRED"
+);
