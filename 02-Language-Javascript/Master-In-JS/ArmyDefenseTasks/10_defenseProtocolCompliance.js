ea95cfb737c0f885b24459f2a29a06a07823f9f6
LINGKON ISLAM <lingkon707@gmail.com>
2026-01-31 18:27:51 +0600

    Create 10_defenseProtocolCompliance.js
    js code

---
+++ 02-Language-Javascript/Master-In-JS/ArmyDefenseTasks/10_defenseProtocolCompliance.js
@@ -0,0 +1,11 @@
+// 10_defenseProtocolCompliance.js
+var protocolBreaches = 1;
+
+console.log(
+  "DEFENSE PROTOCOL CHECK → Breaches:",
+  protocolBreaches,
+  "| ACTION:",
+  protocolBreaches > 0
+    ? "INTERNAL INVESTIGATION INITIATED"
+    : "PROTOCOL MAINTAINED"
+);
