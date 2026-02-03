4ace4240a6c25e3cbe3c6f1ba1f3be89c93e6514
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-03 07:31:27 +0600

    Create 8_finalTopperDecisionSystem.js
    js codes 2

---
+++ 02-Language-Javascript/Master-In-JS/TopStudentFind/8_finalTopperDecisionSystem.js
@@ -0,0 +1,10 @@
+// 8_finalTopperDecisionSystem.js
+var attendanceOk = true;
+var disciplineOk = true;
+var academicTop = "Sadia";
+
+console.log(
+  attendanceOk && disciplineOk
+    ? "FINAL TOPPER APPROVED → " + academicTop
+    : "TOPPER STATUS BLOCKED DUE TO DISCIPLINARY / ATTENDANCE ISSUE"
+);
