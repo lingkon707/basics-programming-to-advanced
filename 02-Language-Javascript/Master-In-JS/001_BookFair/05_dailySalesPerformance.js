dbe65497b7d958baea6a8cc7228c8aa6b488347c
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-09 19:34:39 +0600

    Create 05_dailySalesPerformance.js
    daily sales performance js file

---
+++ 02-Language-Javascript/Master-In-JS/001_BookFair/05_dailySalesPerformance.js
@@ -0,0 +1,11 @@
+// 05_dailySalesPerformance.js
+
+var todaySales = 780000;
+var targetSales = 800000;
+
+console.log(
+  "SALES REPORT → Today:",
+  todaySales,
+  "| TARGET STATUS:",
+  todaySales >= targetSales ? "TARGET ACHIEVED" : "TARGET AT RISK"
+);
