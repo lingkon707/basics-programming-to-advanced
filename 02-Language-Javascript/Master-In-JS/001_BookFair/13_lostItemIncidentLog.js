caa84831884af6ef67c693bca433853d90de648e
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-11 00:36:35 +0600

    Create 13_lostItemIncidentLog.js
    js codes 2

---
+++ 02-Language-Javascript/Master-In-JS/001_BookFair/13_lostItemIncidentLog.js
@@ -0,0 +1,10 @@
+// 13_lostItemIncidentLog.js
+
+var reportsToday = 6;
+
+console.log(
+  "LOST ITEM REPORT → Cases:",
+  reportsToday,
+  "| ACTION:",
+  reportsToday > 5 ? "HELP DESK REINFORCE" : "NORMAL"
+);
