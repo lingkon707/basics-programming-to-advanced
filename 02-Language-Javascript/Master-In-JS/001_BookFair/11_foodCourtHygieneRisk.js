984e3836b36ac2643f5bd08d1dfe28ed36d6f363
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-11 00:35:30 +0600

    Create 11_foodCourtHygieneRisk.js
    risk js

---
+++ 02-Language-Javascript/Master-In-JS/001_BookFair/11_foodCourtHygieneRisk.js
@@ -0,0 +1,9 @@
+// 11_foodCourtHygieneRisk.js
+var hygieneScore = 68;
+
+console.log(
+  "FOOD COURT CHECK → Score:",
+  hygieneScore,
+  "| STATUS:",
+  hygieneScore < 70 ? "TEMPORARY SHUTDOWN" : "APPROVED"
+);
