1589f5009831c0ba69f177287a39837ae2318918
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-13 19:53:39 +0600

    Create 19_exhibitorSatisfactionIndex.js
    satisfied index

---
+++ 02-Language-Javascript/Master-In-JS/001_BookFair/19_exhibitorSatisfactionIndex.js
@@ -0,0 +1,10 @@
+// 19_exhibitorSatisfactionIndex.js
+
+var satisfactionScore = 4.2;
+
+console.log(
+  "EXHIBITOR FEEDBACK →",
+  satisfactionScore >= 4
+    ? "SUCCESSFUL FAIR"
+    : "IMPROVEMENT REQUIRED"
+);
