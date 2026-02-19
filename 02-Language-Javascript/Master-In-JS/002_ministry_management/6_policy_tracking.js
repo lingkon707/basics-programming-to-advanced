ad0e2e86f2a1e873de184ed18e95b80acbb2f5eb
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-19 10:51:45 +0600

    Create 6_policy_tracking.js
    tracking

---
+++ 02-Language-Javascript/Master-In-JS/002_ministry_management/6_policy_tracking.js
@@ -0,0 +1,8 @@
+var policies = [
+  {name: "Digital BD", status: "Running"},
+  {name: "Green City", status: "Pending"}
+];
+
+for (var i = 0; i < policies.length; i++) {
+  console.log(policies[i].name + " Status: " + policies[i].status);
+}
