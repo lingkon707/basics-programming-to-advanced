5a1199c3f6a2e28bebd7392eeb4e6864c67c866a
LINGKON ISLAM <lingkon707@gmail.com>
2026-02-23 13:15:10 +0600

    Create 19_crisis_management.js
    crisis manage

---
+++ 02-Language-Javascript/Master-In-JS/002_ministry_management/19_crisis_management.js
@@ -0,0 +1,8 @@
+var crisisLevel = 4;
+
+switch(crisisLevel) {
+  case 1: console.log("Low Risk"); break;
+  case 2: console.log("Moderate Risk"); break;
+  case 3: console.log("High Risk"); break;
+  case 4: console.log("National Emergency"); break;
+}
