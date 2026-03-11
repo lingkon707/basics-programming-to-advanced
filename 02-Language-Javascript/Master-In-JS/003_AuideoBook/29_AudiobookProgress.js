c7f77e5cdbc7dc1339d38d80f6598afd041c4cc5
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-11 09:32:00 +0600

    Create 29_AudiobookProgress.js
    progress

---
+++ 02-Language-Javascript/Master-In-JS/003_AuideoBook/29_AudiobookProgress.js
@@ -0,0 +1,8 @@
+// Track listening progress percentage
+
+const totalDuration = 300;
+const listened = 90;
+
+const progress = (listened / totalDuration) * 100;
+
+console.log(`Progress: ${progress}%`);
