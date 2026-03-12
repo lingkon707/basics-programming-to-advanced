67b47d61a738cbecc77c8e9235471d7747857816
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-12 18:44:49 +0600

    Create 32_SetAudiobookCategories.js
    categories js

---
+++ 02-Language-Javascript/Master-In-JS/003_AuideoBook/32_SetAudiobookCategories.js
@@ -0,0 +1,9 @@
+// Use Set to avoid duplicate categories
+
+const categories = new Set();
+
+categories.add("Programming");
+categories.add("Programming");
+categories.add("Motivation");
+
+console.log(categories);
