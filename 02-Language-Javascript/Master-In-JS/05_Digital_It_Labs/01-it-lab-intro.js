f0ff715053ff57ad7f07205837b6dc4d4ccd0008
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-15 15:45:32 +0600

    Create 05_Digital_It_Labs/01-it-lab-intro.js
    it labs data

---
+++ 02-Language-Javascript/Master-In-JS/05_Digital_It_Labs/01-it-lab-intro.js
@@ -0,0 +1,9 @@
+var labName = "Digital IT Labs";
+var establishedYear = 2024;
+
+function showLabInfo() {
+    console.log("Welcome to " + labName);
+    console.log("Established in: " + establishedYear);
+}
+
+showLabInfo();
