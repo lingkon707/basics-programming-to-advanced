c1ec25b8370dbf5f5b2bc1b40c1cb16a8998a058
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-16 05:05:44 +0600

    Create 03-course-list.js
    course list

---
+++ 02-Language-Javascript/Master-In-JS/05_Digital_It_Labs/03-course-list.js
@@ -0,0 +1,9 @@
+var courses = ["Web Development", "Graphics Design", "Digital Marketing"];
+
+function showCourses() {
+    for (var i = 0; i < courses.length; i++) {
+        console.log(courses[i]);
+    }
+}
+
+showCourses();
