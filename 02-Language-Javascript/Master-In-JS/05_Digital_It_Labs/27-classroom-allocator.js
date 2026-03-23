2b193a9877c33e8a41f875df1d46337cccf0d058
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-24 01:51:34 +0600

    Create 27-classroom-allocator.js
    allocation

---
+++ 02-Language-Javascript/Master-In-JS/05_Digital_It_Labs/27-classroom-allocator.js
@@ -0,0 +1,12 @@
+var availableRooms = 5;
+
+function allocateRoom() {
+    if (availableRooms > 0) {
+        availableRooms--;
+        console.log("Classroom allocated.");
+    } else {
+        console.log("No classroom available.");
+    }
+}
+
+allocateRoom();
