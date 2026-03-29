8d8d8203a8168a9fce83878c4e2a7c335c3a4659
LINGKON ISLAM <lingkon707@gmail.com>
2026-03-29 11:38:41 +0600

    Create 41-lab-security-check.js
    check js

---
+++ 02-Language-Javascript/Master-In-JS/05_Digital_It_Labs/41-lab-security-check.js
@@ -0,0 +1,11 @@
+var securityEnabled = true;
+
+function checkSecurity() {
+    if (securityEnabled) {
+        console.log("Lab security system is active.");
+    } else {
+        console.log("Security system is disabled.");
+    }
+}
+
+checkSecurity();
