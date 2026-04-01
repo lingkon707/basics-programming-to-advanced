ab6123ffe6682b2b2032480ef5b0c410a0b3f6cf
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-01 13:54:31 +0600

    Create 50-student-certification-log.js
    certificate js

---
+++ 02-Language-Javascript/Master-In-JS/05_Digital_It_Labs/50-student-certification-log.js
@@ -0,0 +1,8 @@
+var certifiedStudents = [];
+
+function addCertifiedStudent(name) {
+    certifiedStudents.push(name);
+    console.log(name + " added to certification log.");
+}
+
+addCertifiedStudent("Karim");
