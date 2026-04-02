6193b9c86128f51c49e88d1ee35545725d83388a
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-02 06:13:22 +0600

    Create 005_University_Accessories/001-Notebook.js
    university accessories playlist

---
+++ 02-Language-Javascript/Master-In-JS/005_University_Accessories/001-Notebook.js
@@ -0,0 +1,11 @@
+// Student Notebook Information
+
+var notebook = {
+  brand: "Classmate",
+  pages: 200,
+  price: 120
+};
+
+console.log("Notebook Brand:", notebook.brand);
+console.log("Total Pages:", notebook.pages);
+console.log("Price:", notebook.price + " BDT");
