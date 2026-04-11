8dc28bf91a92d4c1be4662165fee041fac9fc5b5
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-11 08:02:13 +0600

    Create 027-ResultSheet.js
    sheet

---
+++ 02-Language-Javascript/Master-In-JS/005_University_Accessories/027-ResultSheet.js
@@ -0,0 +1,11 @@
+// Result Sheet Generator
+
+
+var marks = [70, 65, 80];
+var sum = 0;
+
+for (var i = 0; i < marks.length; i++) {
+  sum += marks[i];
+}
+
+console.log("Average:", sum / marks.length);
