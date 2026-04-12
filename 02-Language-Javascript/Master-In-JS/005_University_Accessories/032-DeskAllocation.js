9ca16f10e7e961bcc9a29903cf96de73ebf1f20c
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-12 09:48:55 +0600

    Create 032-DeskAllocation.js
    js desk allocation

---
+++ 02-Language-Javascript/Master-In-JS/005_University_Accessories/032-DeskAllocation.js
@@ -0,0 +1,10 @@
+// Desk Allocation for Students
+
+var students = 45;
+var desks = 50;
+
+if (desks >= students) {
+  console.log("Desk allocation complete");
+} else {
+  console.log("Not enough desks");
+}
