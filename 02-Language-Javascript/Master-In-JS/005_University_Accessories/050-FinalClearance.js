78e0c39df2dac0cb422382d14465d8f70ef7fffb
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-18 09:25:16 +0600

    Create 050-FinalClearance.js
    clearance

---
+++ 02-Language-Javascript/Master-In-JS/005_University_Accessories/050-FinalClearance.js
@@ -0,0 +1,11 @@
+// Final Semester Clearance Status
+
+var libraryClear = true;
+var labClear = true;
+var financeClear = false;
+
+if (libraryClear && labClear && financeClear) {
+  console.log("Clearance complete");
+} else {
+  console.log("Clearance pending");
+}
