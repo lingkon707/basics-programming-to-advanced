a29bbedb42926fbb80839f266f02ad43cb26cbfb
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-26 09:41:46 +0600

    Create 09-refinery-efficiency.js
    efficiency

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/09-refinery-efficiency.js
@@ -0,0 +1,7 @@
+var crudeInput = 100000;
+var refinedOutput = 92000;
+
+var waste = crudeInput - refinedOutput;
+var efficiency = (refinedOutput / crudeInput) * 100;
+
+console.log("Efficiency:", efficiency);
