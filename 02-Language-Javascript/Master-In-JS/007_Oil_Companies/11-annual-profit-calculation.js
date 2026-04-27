02f0107c6d40c4e8a90c3005474c5e6de7b7f008
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-27 11:06:32 +0600

    Create 11-annual-profit-calculation.js
    annual js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/11-annual-profit-calculation.js
@@ -0,0 +1,9 @@
+var revenue = 85000000;
+var expense = 64000000;
+var taxRate = 0.2;
+
+var profit = revenue - expense;
+var tax = profit * taxRate;
+var netProfit = profit - tax;
+
+console.log("Net Profit:", netProfit);
