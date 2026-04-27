1c64f71825e8d5912a992903c826c1af3b3e1a4b
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-27 11:07:29 +0600

    Create 12-drilling-machine-depreciation.js
    machine js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/12-drilling-machine-depreciation.js
@@ -0,0 +1,7 @@
+var machineCost = 2400000;
+var years = 8;
+
+var depreciation = machineCost / years;
+var valueAfter3Years = machineCost - (depreciation * 3);
+
+console.log("Current Value:", valueAfter3Years);
