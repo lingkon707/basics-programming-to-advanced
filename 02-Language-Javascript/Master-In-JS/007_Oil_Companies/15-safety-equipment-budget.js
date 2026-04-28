12e019f84957b40c12b0ff63ba35f5603b2233e6
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-28 10:33:47 +0600

    Create 15-safety-equipment-budget.js
    budget js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/15-safety-equipment-budget.js
@@ -0,0 +1,8 @@
+var workers = 420;
+var kitCost = 3200;
+var reserve = 0.05;
+
+var baseCost = workers * kitCost;
+var totalCost = baseCost + (baseCost * reserve);
+
+console.log("Safety Budget:", totalCost);
