f84ad788b9d4b8acf7089a3b00b0ca2acbe9da4a
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-28 10:32:44 +0600

    Create 14-oil-export-revenue.js
    export js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/14-oil-export-revenue.js
@@ -0,0 +1,8 @@
+var exportBarrels = 76000;
+var exportPrice = 84;
+var shippingCost = 320000;
+
+var revenue = exportBarrels * exportPrice;
+var netRevenue = revenue - shippingCost;
+
+console.log("Export Revenue:", netRevenue);
