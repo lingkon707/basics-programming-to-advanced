9b82bbc4a123b9aef4e8497c6acff5888a2162e4
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-28 10:31:39 +0600

    Create 13-fuel-demand-growth.js
    demand js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/13-fuel-demand-growth.js
@@ -0,0 +1,7 @@
+var currentDemand = 120000;
+var growthRate = 0.07;
+
+var nextYear = currentDemand + (currentDemand * growthRate);
+var twoYearsLater = nextYear + (nextYear * growthRate);
+
+console.log("Future Demand:", twoYearsLater);
