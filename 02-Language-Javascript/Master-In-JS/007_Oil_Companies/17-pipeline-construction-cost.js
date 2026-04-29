e103e43f946b2df267a408445ef354a93951f8cb
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-29 14:23:37 +0600

    Create 17-pipeline-construction-cost.js
    cost js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/17-pipeline-construction-cost.js
@@ -0,0 +1,8 @@
+var lengthKm = 120;
+var costPerKm = 850000;
+
+var constructionCost = lengthKm * costPerKm;
+var contingency = constructionCost * 0.1;
+
+
+console.log("Pipeline Cost:", constructionCost + contingency);
