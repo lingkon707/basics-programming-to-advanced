a05540579f0e318381c720f78380eb884ca7dc6d
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-30 09:46:29 +0600

    Create 21-oil-spill-cleanup-cost.js
    spill js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/21-oil-spill-cleanup-cost.js
@@ -0,0 +1,7 @@
+var area = 1200;
+var costPerUnit = 4500;
+
+var cleanupCost = area * costPerUnit;
+var emergencyFund = cleanupCost * 0.2;
+
+console.log("Total Cleanup:", cleanupCost + emergencyFund);
