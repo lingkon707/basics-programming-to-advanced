4810bc04f25fab3918d353c8042067fc39e52132
LINGKON ISLAM <lingkon707@gmail.com>
2026-04-30 09:45:53 +0600

    Create 20-maintenance-cost-forecast.js
    forecast cost js

---
+++ 02-Language-Javascript/Master-In-JS/007_Oil_Companies/20-maintenance-cost-forecast.js
@@ -0,0 +1,8 @@
+var machines = 48;
+var costPerMachine = 22000;
+var inflationRate = 0.06;
+
+var currentCost = machines * costPerMachine;
+var forecastCost = currentCost + (currentCost * inflationRate);
+
+console.log("Next Year Cost:", forecastCost);
