93b1c1570f928252982f6ed085b1dc6f117e2fe6
LINGKON ISLAM <lingkon707@gmail.com>
2026-05-07 00:40:54 +0600

    Create 14_voter_duplicate_remover.js
    duplicate

---
+++ 02-Language-Javascript/Master-In-JS/008_VoterConsultant/14_voter_duplicate_remover.js
@@ -0,0 +1,11 @@
+function removeDuplicate(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (result.indexOf(arr[i]) === -1) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+console.log(removeDuplicate([1,2,2,3]));
