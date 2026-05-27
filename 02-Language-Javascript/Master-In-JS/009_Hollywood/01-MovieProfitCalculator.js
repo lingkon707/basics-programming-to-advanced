19c2979ad438452ed35ba4b7938ba9c6312bfcfe
LINGKON ISLAM <lingkon707@gmail.com>
2026-05-27 10:49:36 +0600

    Create 009_Hollywood/01-MovieProfitCalculator.js
    profit

---
+++ 02-Language-Javascript/Master-In-JS/009_Hollywood/01-MovieProfitCalculator.js
@@ -0,0 +1,8 @@
+// Concept: function + parameters + return
+
+function calculateProfit(budget, collection) {
+  return collection - budget;
+}
+
+var profit = calculateProfit(150, 600);
+console.log("Movie Profit:", profit, "Million USD");
