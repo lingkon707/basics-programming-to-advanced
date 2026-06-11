19e8152d859e55a0f221f52fe443eaf9f562fcb8
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-11 10:20:51 +0600

    Create ielts-score-calculator.js
    score

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-score-calculator.js
@@ -0,0 +1,8 @@
+// IELTS overall band score calculation (average)
+var listening = 7.5;
+var reading = 6.5;
+var writing = 6.0;
+var speaking = 7.0;
+
+var average = (listening + reading + writing + speaking) / 4;
+console.log("Overall IELTS Band Score:", average.toFixed(1));
