fbe9b75418329849b78110c3808247d1c3a78013
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-11 10:21:11 +0600

    Create ielts-band-check.js
    band

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-band-check.js
@@ -0,0 +1,8 @@
+// Check if IELTS band is eligible for abroad study
+var bandScore = 6.5;
+
+if (bandScore >= 6.0) {
+    console.log("Eligible for most universities");
+} else {
+    console.log("Not eligible, improve your band score");
+}
