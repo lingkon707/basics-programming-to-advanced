be14aacb4e59c6a0c949caec7c866ad4fd6e4c6c
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-11 10:21:40 +0600

    Create ielts-module-type.js
    module

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-module-type.js
@@ -0,0 +1,8 @@
+// IELTS module type selection
+var module = "Academic";
+
+if (module === "Academic") {
+    console.log("Chosen Module: Academic IELTS");
+} else {
+    console.log("Chosen Module: General Training IELTS");
+}
