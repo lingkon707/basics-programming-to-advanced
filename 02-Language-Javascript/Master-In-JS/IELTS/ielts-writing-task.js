2d4cb56746e730dd1a961e5647a8f6c700f7ee83
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-12 00:34:27 +0600

    Create ielts-writing-task.js
    writing

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-writing-task.js
@@ -0,0 +1,14 @@
+// IELTS Writing tasks
+
+var writingTask = 2;
+
+switch (writingTask) {
+    case 1:
+        console.log("Task 1: Report or Letter Writing");
+        break;
+    case 2:
+        console.log("Task 2: Essay Writing");
+        break;
+    default:
+        console.log("Invalid Task");
+}
