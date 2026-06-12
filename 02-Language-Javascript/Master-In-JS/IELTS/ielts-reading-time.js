54ef04f0378671ba2b659bb36dd271dafe3f44c4
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-13 00:58:06 +0600

    Create ielts-reading-time.js
    reading time

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-reading-time.js
@@ -0,0 +1,7 @@
+// IELTS Reading time management
+
+var totalTime = 60; // minutes
+var passages = 3;
+
+var timePerPassage = totalTime / passages;
+console.log("Time per passage:", timePerPassage, "minutes");
