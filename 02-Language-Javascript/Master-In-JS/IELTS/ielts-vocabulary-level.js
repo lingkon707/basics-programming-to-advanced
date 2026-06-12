cc8ccca159e7d45aaaee33ce3eade57575153ba1
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-13 00:58:46 +0600

    Create ielts-vocabulary-level.js
    level js

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-vocabulary-level.js
@@ -0,0 +1,9 @@
+// IELTS vocabulary level check
+
+var vocabularyLevel = "Intermediate";
+
+if (vocabularyLevel === "Advanced") {
+    console.log("Good vocabulary for Band 7+");
+} else {
+    console.log("Need to improve vocabulary");
+}
