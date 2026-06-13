acf1c87550246960861836a2c2c59e98af5fcce5
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-14 01:03:00 +0600

    Create ielts-university-eligibility.js
    eligibility js

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/ielts-university-eligibility.js
@@ -0,0 +1,10 @@
+// ielts-university-eligibility.js
+
+var overall = 6.5;
+var writing = 5.5;
+
+if (overall >= 6.5 && writing >= 6) {
+    console.log("Eligible for university");
+} else {
+    console.log("Not eligible");
+}
