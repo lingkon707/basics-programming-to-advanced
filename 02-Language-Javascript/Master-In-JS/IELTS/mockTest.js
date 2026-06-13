d5bc8d2a0450916968ff568140b71ddafb22eb8f
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-14 01:01:09 +0600

    Create mockTest.js
    mock test

---
+++ 02-Language-Javascript/Master-In-JS/IELTS/mockTest.js
@@ -0,0 +1,7 @@
+// IELTS mock test result
+
+var correctAnswers = 30;
+var totalQuestions = 40;
+
+var percentage = (correctAnswers / totalQuestions) * 100;
+console.log("Mock Test Score:", percentage + "%");
