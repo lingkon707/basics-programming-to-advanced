552682ea4d30742a7786d7e35a5180d1e136050a
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-18 00:07:43 +0600

    Create 015-revenue-tracker.js
    track revenue

---
+++ 02-Language-Javascript/Master-In-JS/010_Shoes_Brand/015-revenue-tracker.js
@@ -0,0 +1,8 @@
+// 015-revenue-tracker.js
+
+var orders = [100, 200, 300];
+var revenue = 0;
+
+for (var i = 0; i < orders.length; i++) revenue += orders[i];
+
+console.log("Total Revenue: " + revenue);
