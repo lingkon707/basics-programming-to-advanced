92be60b80fc8634c37994d1175452dba6b122801
LINGKON ISLAM <lingkon707@gmail.com>
2026-06-30 00:12:27 +0600

    Create add-and-remove.js
    add, remove

---
+++ 02-Language-Javascript/Master-In-JS/012_Inventory/add-and-remove.js
@@ -0,0 +1,15 @@
+// File: 24-product-remove.js
+var productArr = ["Laptop", "Mouse"];
+
+productArr.pop();
+
+console.log("After Remove:", productArr);
+
+
+
+// File: 25-product-add.js
+var productList = [];
+
+productList.push("Keyboard");
+
+console.log("Product Added:", productList);
