493a8f535e8be989b9157ac6539935caf0b0ba8e
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-02 00:24:20 +0600

    Create 09__inventory_value_sum.js
    sum

---
+++ 02-Language-Javascript/Master-In-JS/013_JewelryTopic/09__inventory_value_sum.js
@@ -0,0 +1,9 @@
+var items = [125000, 98000, 156000, 210000];
+
+var totalValue = 0;
+
+for (var i = 0; i < items.length; i++) {
+  totalValue += items[i];
+}
+
+console.log("Total Inventory Value:", totalValue);
