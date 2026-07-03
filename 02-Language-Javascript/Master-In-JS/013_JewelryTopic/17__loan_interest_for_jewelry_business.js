db9f042431def51bbeeae652a8159262512e40a5
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-03 12:28:03 +0600

    Create 17__loan_interest_for_jewelry_business.js
    loan

---
+++ 02-Language-Javascript/Master-In-JS/013_JewelryTopic/17__loan_interest_for_jewelry_business.js
@@ -0,0 +1,7 @@
+var loanAmount = 2000000;
+var annualRate = 9;
+var timeYears = 2;
+
+var interest = (loanAmount * annualRate * timeYears) / 100;
+
+console.log("Total Interest Payable:", interest);
