2ccc0048521b658200144e825c2cbe0f28211d8e
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-04 14:48:45 +0600

    Create 21__jewelry_weight_balance_check.js
    balance

---
+++ 02-Language-Javascript/Master-In-JS/013_JewelryTopic/21__jewelry_weight_balance_check.js
@@ -0,0 +1,8 @@
+var components = [4.2, 3.1, 2.7];
+var total = 0;
+
+for (var i = 0; i < components.length; i++) {
+  total += components[i];
+}
+
+console.log("Total Jewelry Weight:", total, "gm");
