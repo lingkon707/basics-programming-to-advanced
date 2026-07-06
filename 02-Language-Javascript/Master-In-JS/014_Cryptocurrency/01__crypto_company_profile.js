f9e114a9402440208d217881b30e9bba54a8acb6
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-06 07:41:57 +0600

    Create 014_Cryptocurrency/01__crypto_company_profile.js
    cryptocurrency js

---
+++ 02-Language-Javascript/Master-In-JS/014_Cryptocurrency/01__crypto_company_profile.js
@@ -0,0 +1,9 @@
+var company = {
+    name: "CryptoNova",
+    founded: 2019,
+    countries: ["USA", "Singapore", "UAE"],
+    active: true
+};
+
+var status = company.active ? "Operational" : "Inactive";
+console.log("Company:", company.name, "| Status:", status);
