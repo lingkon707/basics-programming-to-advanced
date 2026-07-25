dc9bc140bd5390e9d8bd216c4900acbf93ccacbf
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-25 07:50:00 +0600

    Create 05__student_grade_calculator.js
    grade students

---
+++ 02-Language-Javascript/Master-In-JS/016_ StudentResultsBoard/05__student_grade_calculator.js
@@ -0,0 +1,8 @@
+function grade(per) {
+    if (per >= 90) return "A+";
+    if (per >= 80) return "A";
+    if (per >= 70) return "B";
+    if (per >= 60) return "C";
+    if (per >= 50) return "D";
+    return "F";
+}
