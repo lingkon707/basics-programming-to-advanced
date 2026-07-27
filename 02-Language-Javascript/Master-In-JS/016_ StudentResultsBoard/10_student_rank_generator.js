0271836f709672ca8f58dc906047df3c6d27c77d
LINGKON ISLAM <lingkon707@gmail.com>
2026-07-27 10:53:48 +0600

    Create 10_student_rank_generator.js
    rank

---
+++ 02-Language-Javascript/Master-In-JS/016_ StudentResultsBoard/10_student_rank_generator.js
@@ -0,0 +1,8 @@
+function rank(per) {
+    if (per >= 90) return 1;
+    if (per >= 80) return 2;
+    if (per >= 70) return 3;
+    if (per >= 60) return 4;
+    if (per >= 50) return 5;
+    return 6;
+}
