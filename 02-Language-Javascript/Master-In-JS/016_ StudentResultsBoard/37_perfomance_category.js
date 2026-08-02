1f33bec8c11f9b7157a5a1facfd1d779f3a821ed
LINGKON ISLAM <lingkon707@gmail.com>
2026-08-02 10:47:52 +0600

    Create 37_perfomance_category.js
    performance js

---
+++ 02-Language-Javascript/Master-In-JS/016_ StudentResultsBoard/37_perfomance_category.js
@@ -0,0 +1,7 @@
+function performanceCat(per) {
+    if (per >= 90) return "Excellent";
+    if (per >= 75) return "Very Good";
+    if (per >= 60) return "Good";
+    if (per >= 45) return "Satisfactory";
+    return "Needs Improvement";
+}
